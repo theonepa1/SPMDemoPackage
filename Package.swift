73a0409851e4bd75227f8972c823482419696713
Pavan Mellamputi <pavan.mellamputi@socure.com>
2022-12-09 13:54:32 -0800

    First commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+let package = Package(
+    name: "SPMDemoPackage",
+    platforms: [
+           .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SPMDemo",
+            targets: ["SPMDemoPackage"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/datatheorem/TrustKit.git", from: "2.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DeviceRisk",
+            url: "https://sdk.socure.com/socure-sdks/sigma-device/ios/socure-device-risk-2.0.0.zip",
+            checksum: "1b22082e3d9adb82107e015b1c513a57ceac1523286def535d01e0ba4416b264"
+        ),
+        .target(
+            name: "SPMDemoPackageWrapper",
+            dependencies: [
+                .target(name: "DeviceRisk"),
+                .product(name: "TrustKit", package: "TrustKit")
+            ],
+            path: "SPMDemoPackageWrapper"
+        ),
+    ]
+)
