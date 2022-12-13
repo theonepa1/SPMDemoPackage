9390688fbe06de92f046e255f46bab908e1ba286
theonepa1 <theone.pa1@gmail.com>
2022-12-13 09:53:47 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,21 +12,22 @@ let package = Package(
             name: "SPMDemo",
             targets: ["SPMDemoPackageWrapper"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/datatheorem/TrustKit.git", from: "2.0.0"),
-    ],
     targets: [
         .binaryTarget(
             name: "DeviceRisk",
             url: "https://github.com/theonepa1/SPMDemoPackage/raw/main/socure-device-risk-2.0.1.zip",
             checksum: "113278382469bfa045237fa2c99980c0c861741c5a52a044ab482df4ff6d37ae"
         ),
+        .binaryTarget(
+            name: "DeviceRisk",
+            url: "https://github.com/theonepa1/SPMDemoPackage/raw/main/TrustKit.xcframework.zip",
+            checksum: "22e56103b3399feaac9c2ce86bd17c3bb8f2d012ec7644b6dc38886b15a5c821"
+        ),
         .target(
             name: "SPMDemoPackageWrapper",
             dependencies: [
                 .target(name: "DeviceRisk"),
-                .product(name: "TrustKit", package: "TrustKit")
+                .target(name: "TrustKit")
             ],
             path: "SPMDemoPackageWrapper"
         ),
