3093f3899a98bd7556e6dfea4bb4270529a35370
theonepa1 <theone.pa1@gmail.com>
2023-06-06 15:45:38 -0700

    Add files via upload

---
+++ Frameworks/DeviceRisk.xcframework/ios-arm64_x86_64-simulator/DeviceRisk.framework/Headers/Loader.h
@@ -0,0 +1,17 @@
+//
+//  Loader.h
+//  DeviceRisk
+//
+//  Created by Pavan Mellamputi on 10/30/22.
+//  Copyright © 2022 Socure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Loader : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
