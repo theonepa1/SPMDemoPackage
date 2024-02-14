9652b7cab0bd5e35467daabde0ff4d7a9795ac68
theonepa1 <theone.pa1@gmail.com>
2024-02-13 19:40:00 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,27 +1,23 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+
 let package = Package(
     name: "SPMDemoPackage",
     platforms: [
-           .iOS(.v13)
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: "SPMDemo",
-            targets: ["SPMDemoPackageWrapper"])
+            name: "DeviceRisk",
+            targets: ["DeviceRisk"])
     ],
     targets: [
         .binaryTarget(
             name: "DeviceRisk",
-            path: "Frameworks/DeviceRisk.xcframework"
-        ),
-        .target(
-            name: "SPMDemoPackageWrapper",
-            dependencies: [
-                .target(name: "DeviceRisk"),
-            ],
-            path: "SPMDemoPackageWrapper"
-        ),
+            url: "https://sdk.socure.com/socure-sdks/sigma-device/ios/socure-device-risk-4.0.0.zip",
+            checksum: "449cca921d09cfc18f890719ae9363a7054e685467a10a41c7c8085b2e3927de"
+        )
     ]
 )
